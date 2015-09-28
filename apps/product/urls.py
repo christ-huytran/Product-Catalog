7e54d7ee2d108328d86dd902588562ed8124a414
Chris Tran <christopher.huytran@gmail.com>
2015-09-28 09:02:32 -0700

    users can add products

--- apps/product/urls.py
+++ apps/product/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url
 from apps.product import views
 urlpatterns = patterns('',
 	url(r'^$', views.index, name='index'),
-	url(r'^(?P<product_id>\d+)/$', views.show, name='show')
+	url(r'^(?P<product_id>\d+)/$', views.show, name='show'),
+	url(r'^create$', views.create, name='create')
 )
--- apps/product/views.py
+++ apps/product/views.py
@@ -1,12 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from apps.product.models import Product, Manufacturer
 # Create your views here.
 def index(request):
 	products = Product.objects.all()
+	manufacturers = Manufacturer.objects.all()
 	context = {
-		'products': products
+		'products': products,
+		'manufacturers': manufacturers
 	}
 	return render(request, 'product/index.html', context)
 
 def show(request, product_id):
 	return render(request, 'product/show.html')
+
+def create(request):
+	manufacturer = Manufacturer.objects.get(id = request.POST['manufacturer'])
+	new_product = Product(manufacturer = manufacturer, product_name = request.POST['product_name'], price = request.POST['price'])
+	new_product.save()
+	return redirect('index')
