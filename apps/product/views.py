50311cb59bf3cc60a55993d1bf8e2c61698f9aba
Chris Tran <christopher.huytran@gmail.com>
2015-09-27 21:09:35 -0700

    display products on index view

--- apps/product/views.py
+++ apps/product/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
+from apps.product.models import Product, Manufacturer
 # Create your views here.
 def index(request):
-	return render(request, 'product/index.html')
+	products = Product.objects.all()
+	context = {
+		'products': products
+	}
+	return render(request, 'product/index.html', context)
 
 def show(request, product_id):
 	return render(request, 'product/show.html')
