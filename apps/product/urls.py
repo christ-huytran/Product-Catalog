2194dc6dd13cad05a72ddc920f85daad1694235c
Chris Tran <christopher.huytran@gmail.com>
2015-09-28 11:21:44 -0700

    users can update products

--- apps/product/urls.py
+++ apps/product/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, url
 from apps.product import views
 urlpatterns = patterns('',
 	url(r'^$', views.index, name='index'),
-	url(r'^(?P<product_id>\d+)/$', views.show, name='show'),
-	url(r'^create$', views.create, name='create')
+	url(r'^(?P<product_id>\d+)$', views.show, name='show'),
+	url(r'^create$', views.create, name='create'),
+	url(r'^update/(?P<product_id>\d+)$', views.update, name='update')
 )
--- apps/product/views.py
+++ apps/product/views.py
@@ -11,10 +11,25 @@ def index(request):
 	return render(request, 'product/index.html', context)
 
 def show(request, product_id):
-	return render(request, 'product/show.html')
+	product = Product.objects.get(id = product_id)
+	manufacturers = Manufacturer.objects.all()
+	context = {
+		'product': product,
+		'manufacturers': manufacturers
+	}
+	return render(request, 'product/show.html', context)
 
 def create(request):
 	manufacturer = Manufacturer.objects.get(id = request.POST['manufacturer'])
 	new_product = Product(manufacturer = manufacturer, product_name = request.POST['product_name'], price = request.POST['price'], description = request.POST['description'])
 	new_product.save()
+	return redirect('index')
+
+def update(request, product_id):
+	product = Product.objects.get(id = product_id)
+	product.product_name = request.POST['product_name']
+	product.price = request.POST['price']
+	product.description = request.POST['description']
+	product.manufacturer = Manufacturer.objects.get(id = request.POST['manufacturer'])
+	product.save()
 	return redirect('index')
